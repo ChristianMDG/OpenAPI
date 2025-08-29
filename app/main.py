8e426b32d0f947ade43a942d528daaecc89ce4b8
ChristianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2025-08-29 17:21:16 +0300

    exo 2 finish without fastAPI

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-#exo 1 Ping
+#exo 1 Ping : 
 
 @app.get("/ping", summary="Retourne pong")
 async def get_pong():
     return "pong"
 
+
+class User(BaseModel):
+    id: int
+    name: str
+    email:str
+users_db: List = []
+
+#Exercice 2 – Manipuler les paramètres et les réponses: 
+@app.get("/users",summary="Récupère la liste des utilisateurs avec pagination")
+async def get_users(page:int ):
+    return users_db
